6f369d812a81a84060804b78cdc05d3fedc1c6f1
Rodrigo Santos Oliveira <rodrigoliveirasantos11@gmail.com>
2024-09-01 13:49:08 -0300

    feat: Criação da página weather-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'weather',
+    component: WeatherHomeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'weather',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
